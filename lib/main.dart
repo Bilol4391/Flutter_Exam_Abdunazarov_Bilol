3507908fd05163b27fb93cfa3f19752e63f9ab16
Bilol Abdunazarov <bilolabdunazarov8480gmail.com@Bilols-MacBook-Pro.local>
2022-12-05 22:17:30 +0500

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainFile());
+}
+
+class MainFile extends StatelessWidget {
+  const MainFile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ,
+    );
+  }
+}
